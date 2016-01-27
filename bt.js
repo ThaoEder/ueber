aa3190a9d3943202d7faf11f4f4aed900e1cf17c
thaoeder <ederthao@web.de>
2016-01-27 22:54:38 +0000

    first push

---
+++ bt.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+    
+}
+
+		function googleTranslateElementInit() {
+		new google.translate.TranslateElement({pageLanguage: 'en', autoDisplay: false, multilanguagePage: true}, 'google_translate_element');
+		}
